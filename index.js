27358804c3410b5de53381a4e8a78f44217662c2
kalbert83 <mat17031@byui.edu>
2019-11-14 14:20:52 -0700

    new html

--- index.js
+++ index.js
@@ -8,3 +8,37 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+    
+  app.get("/calculate", handleCalculation);
+
+  function handleCalculation(req, res) {
+    const weight = Number(request.query.weight);
+    const pakageType = request.query.pakage;
+
+    computeCalculation(weight, pakageType);
+  }
+  function calculateRate (weight, packageType) {
+    let result = 0;
+    if (packageType = "Letters (Stamped)") {
+      result = .40 + (weight * .15);
+    }
+    else if (packageType = "Letters (Metered)") {
+      result = .35 + (weight * .15);
+    }
+    else if (packageType = "Large Envelopes (Flats)") {
+      result = .85 + (weight * .15);
+    }
+    else if (packageType = "First-Class Package Service—Retai") {
+      if (weight <= 4) { result = 3.66; }
+      else if (weight <= 8) { result = 4.39; }
+      else if (weigth <= 12) { result = 5.19; }
+      else {result = 5.71}
+    }
+    else {
+      result = 'Error';
+    }
+    const params = {result : result};
+    res.render('pages/content', params);
+  }
+
+  
