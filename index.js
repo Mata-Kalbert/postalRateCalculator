2f79b659f27bea777dc6257f6a7f9eec7b1363b2
kalbert83 <mat17031@byui.edu>
2019-11-14 22:58:25 -0700

    update

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ const port = process.env.PORT || 5000;
 app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
+app.get('/', (req, res) => res.render('pages/index'));
 app.get('/calculate', handleCalculation);
 app.listen(port, function() {
   console.log('Node app is running on port', port);
